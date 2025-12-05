b7a0c0ac075c43fb8c9c3a2a4d3400459a9fb99e
Utkarsh bhola <utkarsh.bhola@gmail.com>
2025-12-05 18:37:15 +0530

    added the full working project

---
+++ backend/collector/src/main/kotlin/com/observability/collector/config/MongoConfig.kt
@@ -0,0 +1,27 @@
+package com.observability.collector.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.MongoDatabaseFactory
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory
+
+@Configuration
+class MongoConfig {
+
+    @Bean
+    fun logsDbFactory(): MongoDatabaseFactory =
+        SimpleMongoClientDatabaseFactory("mongodb://localhost:27017/logsDB")
+
+    @Bean
+    fun logsMongoTemplate(): MongoTemplate =
+        MongoTemplate(logsDbFactory())
+
+    @Bean
+    fun metaDbFactory(): MongoDatabaseFactory =
+        SimpleMongoClientDatabaseFactory("mongodb://localhost:27017/metaDB")
+
+    @Bean
+    fun metaMongoTemplate(): MongoTemplate =
+        MongoTemplate(metaDbFactory())
+}
---
+++ backend/collector/src/main/kotlin/com/observability/collector/config/MongoMetaConfig.kt
@@ -0,0 +1,2 @@
+package com.observability.collector.config
+
